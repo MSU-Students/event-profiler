6952f2b9f608f67a801a0271deb750ea9d642995
fencim <fencim@gmail.com>
2024-03-09 13:47:52 +0800

    firebase configuration init

---
+++ src/services/firebase.service.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBq4HqgxHDMkqLwSR2qfDvsTDzjp679ccI",
+  authDomain: "event-wheeler.firebaseapp.com",
+  projectId: "event-wheeler",
+  storageBucket: "event-wheeler.appspot.com",
+  messagingSenderId: "917549826899",
+  appId: "1:917549826899:web:b9483e42a0d618f4a297a6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

ffaf4b64f0a1a0ccf5705fccec3a768b5d87aa6c
fencim <fencim@gmail.com>
2024-03-10 08:33:46 +0800

    fix login

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -16,19 +16,11 @@ async function login() {
 <template>
   <q-card class="bg-primary fixed-center">
     <q-card-section class="text-h6">
-      Login Page
+      Welcome to Event Wheeler
     </q-card-section>
-    <q-card-section>
-      <q-input v-model="username" />
-      <q-input v-model="password" type="password"  />
-    </q-card-section>
-    <q-banner>
-      {{ username + password }}
-    </q-banner>
     <q-card-actions>
       <q-btn icon="login" @click="login">Login with Google</q-btn>
-      <q-btn icon="login">Login</q-btn>
-      <q-btn icon="cancel" to="/" >No</q-btn>
+      <q-btn icon="home" to="/" ></q-btn>
     </q-card-actions>
   </q-card>
 </template>
